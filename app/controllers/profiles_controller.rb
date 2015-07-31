44da4faf32b78549b7468165db858c66226ac2ab
David Alvarado <davidealva@gmail.com>
2015-07-30 20:49:28 -0600

    fixed home page

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,7 +3,7 @@ class ProfilesController < ApplicationController
 	def new
 		# form for user to fill out new profile
 		@user = User.find(params[:user_id])
-		@profile = @user.build_profile
+		@profile = Profile.new
 	end
 
 	def create
