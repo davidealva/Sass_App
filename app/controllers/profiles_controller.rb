d924f9ca4832c010d7031ff5bcaebfbe1736b5ba
David Alvarado <davidealva@gmail.com>
2015-07-30 19:56:37 -0600

    created profile form

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,3 +1,26 @@
 class ProfilesController < ApplicationController
 
+	def new
+		# form for user to fill out new profile
+		@user = User.find(params[:user_id])
+		@profile = @user.build_profile
+	end
+
+	def create
+		@user = User.find(params[:user_id])
+		@profile = @user.build_profile(profile_params)
+		if @profile.save
+			flash[:success] = "Your profile has been created"
+			redirect_to user_path(params[:user_id])
+		else
+			render action: :new 
+		end
+	end
+
+	private
+
+		def profile_params
+			params.require(:profile).permit(:first_name, :last_name, :title, :phone_number, :contact_email, :description)
+		end
+
 end
